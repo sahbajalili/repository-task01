58dfa41f86d2f20b95260dd26b293327db5f6ba7
sahbajalili <sahbajalili@gmail.com>
2024-07-14 17:39:52 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let doc = document
 
+let body = doc.querySelector('body')
 let cover1 = doc.querySelector('.ri-mail-fill')
 let cover2 = doc.querySelector('.ri-smartphone-line')
 let cover3 = doc.querySelector('.ri-map-pin-line')
 
-console.log(cover1)
-console.log(cover2)
-console.log(cover3)
-
+body.addEventListener('touchstart' , touchBody)
 cover1.addEventListener('touchstart' , touch1)
 cover2.addEventListener('touchstart' , touch2)
 cover3.addEventListener('touchstart' , touch3)
+
+function touchBody() {
+    cover1.removeEventListener('touchstart' , touch1)
+    cover2.removeEventListener('touchstart' , touch2)
+    cover3.removeEventListener('touchstart' , touch3)
+}
 
 function touch1() {
     cover1.style.borderBottomLeftRadius = '0px'
