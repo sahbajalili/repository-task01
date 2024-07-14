d0dcbc2a6b56a1dad93a4338f67cc31d11fcd306
sahbajalili <sahbajalili@gmail.com>
2024-07-14 16:01:44 +0430

    edit

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 let doc = document
 
 let box = doc.querySelector('.box')
-let cover = doc.querySelector('.cover')
+let cover1 = doc.querySelector('.1')
+let cover2 = doc.querySelector('.2')
+let cover3 = doc.querySelector('.3')
 
-cover.addEventListener('touchstart' , touch)
+cover1.addEventListener('touchstart' , touch1)
+cover2.addEventListener('touchstart' , touch2)
+cover3.addEventListener('touchstart' , touch3)
 
-function touch() {
-    cover.style.borderBottomLeftRadius = '0px'
-    cover.style.borderBottomRightRadius = '0px'
-    cover.style.transform = 'transtranslateY(-30px)'
+function touch1() {
+    cover1.style.borderBottomLeftRadius = '0px'
+    cover1.style.borderBottomRightRadius = '0px'
+    cover1.style.transform = 'transtranslateY(-30px)'
+}
+
+function touch2() {
+    cover2.style.borderBottomLeftRadius = '0px'
+    cover2.style.borderBottomRightRadius = '0px'
+    cover2.style.transform = 'transtranslateY(-30px)'
+}
+
+function touch3() {
+    cover3.style.borderBottomLeftRadius = '0px'
+    cover3.style.borderBottomRightRadius = '0px'
+    cover3.style.transform = 'transtranslateY(-30px)'
 }
